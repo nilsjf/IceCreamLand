f80309c6fc6b8a6d5cb02e880fa244ccb45aa685
nilsjf <nils.figura@gmail.com>
2020-10-05 10:52:42 +0200

    Klasse BestellPosition hinzugefügt

---
+++ IceCreamLand/BestellPosition.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IceCreamLand
+{
+    class BestellPosition
+    {
+        private int anzahl;
+
+        public int Anzahl
+        {
+            get { return anzahl; }
+            set
+            {
+                if (value > 0) anzahl = value;
+                else throw new ArgumentException("Anzahl muss mind. 1 sein!");
+            }
+        }
+
+        public int PositionsNr { get; set; }
+
+        public double ServiceKostenInEuro
+        {
+            get { return Anzahl * 0.5; }
+        }
+
+        public BestellPosition(int anzahl)
+        {
+            Anzahl = anzahl;
+        }
+    }
+}
--- IceCreamLand/Program.cs
+++ IceCreamLand/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace IceCreamLand
 {
@@ -6,7 +7,30 @@ namespace IceCreamLand
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<BestellPosition> liste = new List<BestellPosition>
+                    {
+                        new BestellPosition(1),
+                        new BestellPosition(2),
+                        new BestellPosition(3),
+                        new BestellPosition(10),
+                    };
+            int count = 1;
+            foreach (var element in liste)
+            {
+                Console.WriteLine("Datentyp: " + element.GetType());
+                Console.WriteLine("Menge: " + element.Anzahl);
+                Console.WriteLine("Servicekosten in EUR: " + Math.Round(element.ServiceKostenInEuro, 2).ToString("0.00"));
+                element.PositionsNr = count;
+                count++;
+                Console.WriteLine("PositionsNr (Zusatzaufgabe): " + element.PositionsNr);
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+
+            //Zusatzaufgabe: Objektinitialisierer in C#:
+
+            // https://docs.microsoft.com/de-de/dotnet/csharp/programming-guide/classes-and-structs/how-to-initialize-objects-by-using-an-object-initializer       
+
         }
     }
 }
